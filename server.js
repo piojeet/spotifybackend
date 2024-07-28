ad8b5215e28ee64549763053341a1fea735d55f7
piojeet <manjhipiojeet@gmail.com>
2024-07-28 18:17:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import songRouter from './scr/routes/songRoutes.js';
+import connectDB from './scr/config/mongodb.js';
+import connectCloudinary from './scr/config/cloudinary.js';
+import albumRouter from './scr/routes/albumRoutes.js';
+
+
+// app config 
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+connectCloudinary();
+
+// middlewares 
+app.use(express.json());
+app.use(cors());
+
+// initailizing routes
+app.use("/api/song",songRouter)
+app.use("/api/album",albumRouter)
+
+
+app.get('/',(req,res)=> res.send("API working"))
+
+app.listen(port,()=>console.log(`Server started on ${port}`))
